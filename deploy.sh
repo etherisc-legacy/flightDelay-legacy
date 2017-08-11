0fedef8a86dd9751425f4beb729a1accc83a5c0a
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2017-08-11 09:45:49 +0200

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "Deploying to" $1
+
+echo "Installing Parity"
+bash <(curl https://get.parity.io -Lk)
+
+echo "Installing NPM packages"
+npm install
+
+echo "Preprocessing"
+APP_ID=$FLIGHT_STAT_APP_ID APP_KEY=$FLIGHT_STAT_APP_KEY ./preprocess.sh $1
+
+echo "Getting author"
+author=$(node -e "console.log(require('./truffle.js').networks['$1'].from)")
+echo "Author:" $author
+
+echo "Decrypting keys"
+openssl aes-256-cbc -K $encrypted_d265c45176be_key -iv $encrypted_d265c45176be_iv -in keys.tar.enc -out keys.tar -d
+
+echo "Extracting keys"
+tar xvf keys.tar
+
+echo "Running Parity"
+parity --author $author --chain $1 --unlock $author --password ./keys/$1 --keys-path ./keys/ --mode active
+sleep 5
+
+echo "Synchronising with network"
+until curl --data '{"jsonrpc":"2.0","method":"eth_syncing", "id":1}' -H "Content-Type: application/json" -s localhost:8545 | grep 'false'
+do
+  curl --data '{"jsonrpc":"2.0","method":"eth_syncing", "id":1}' -H "Content-Type: application/json" localhost:8545
+  sleep 1
+done
+
+echo "Selecting migrations"
+./migselect.sh
+
+echo "Selecting tests"
+./testselect.sh
+
+echo "Running tests"
+npm test --network $1
+
+echo "Deploying"
+npm run deploy --network $1
