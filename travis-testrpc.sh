4ff4cd6b03c8b56d827942a4f969b9dcbe1237b4
Константин Андрианов <andrianov.dev@yandex.by>
2017-08-13 10:47:29 +0200

    fix

--- travis-testrpc.sh
+++ travis-testrpc.sh
@@ -2,10 +2,13 @@
 
 APP_ID=$FLIGHT_STAT_APP_ID APP_KEY=$FLIGHT_STAT_APP_KEY ./preprocess.sh testrpc
 npm run testrpc &
-cd external/ethereum-bridge && npm install && node bridge -a 0 -H localhost:9545 &
+cd external/ethereum-bridge
+npm install
+node bridge -a 0 -H localhost:9545 &
 
+cd ../../
 echo "Wait bridge"
-until cat ./external/ethereum-bridsge/bridge.log | grep "Listening @"
+until cat ./external/ethereum-bridge/bridge.log | grep "Listening @"
 do
   echo -n .
   sleep 1
