b06db5884b50f63310985c59dae17b0d5668f69c
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2017-08-14 22:23:25 +0200

    make sure script is not failing

--- travis-testrpc.sh
+++ travis-testrpc.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e
+
 APP_ID=$FLIGHT_STAT_APP_ID APP_KEY=$FLIGHT_STAT_APP_KEY ./preprocess.sh testrpc
 npm run testrpc &
 cd external/ethereum-bridge
 npm install
 node bridge -a 0 -H localhost:9545 &
-
 cd ../../
+
 echo "Wait bridge"
 until cat ./external/ethereum-bridge/bridge.log | grep "Listening @"
 do
@@ -16,11 +18,14 @@ done
 
 ./migselect.sh
 npm run compile-testrpc
+
 ./test-get.sh Test_FlightDelayController.js
 npm test -- --network testrpc
+
 ./test-get.sh Test_FlightDelayDestruct.js
 npm test -- --network testrpc
+
 ./test-get.sh Test_FlightDelayNewPolicy.js
 npm test -- --network testrpc
-ps -ef | grep node
+
 killall -9 node
