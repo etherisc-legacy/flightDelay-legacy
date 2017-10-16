43e8c45fa0256172f1e89f8c5ccd1082c8a534c1
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2017-10-16 15:42:00 +0300

    fix path to artifact

--- ci-cd/set-contract-address.js
+++ ci-cd/set-contract-address.js
@@ -1,7 +1,7 @@
 const Web3 = require('web3');
 
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-const newPolicy = require('./build/contracts/FlightDelayNewPolicy.json');
+const newPolicy = require('../build/contracts/FlightDelayNewPolicy.json');
 
 const abi = [
     {
